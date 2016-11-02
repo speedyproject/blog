f5974e1995e5cc49e213d54759ffa27b732a35b3
Nico W. J <gksqlsdy@gmail.com>
2016-11-02 18:11:41 +0800

    Nico W. J
    Add Xorm support

--- app/init.go
+++ app/init.go
@@ -1,6 +1,9 @@
 package app
 
-import "github.com/revel/revel"
+import (
+	"github.com/revel/revel"
+	"blog/app/support"
+)
 
 func init() {
 	// Filters is the default set of global filters.
@@ -21,7 +24,7 @@ func init() {
 
 	// register startup functions with OnAppStart
 	// ( order dependent )
-	// revel.OnAppStart(InitDB)
+	revel.OnAppStart(support.InitXorm)
 	// revel.OnAppStart(FillCache)
 }
 

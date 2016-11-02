e648fb42d5da8e7503751a08582b7ee949e7b9c2
Nico W. J <gksqlsdy@gmail.com>
2016-11-02 22:38:31 +0800

    Add AesEncrypt

--- app/support/sign.go
+++ app/support/sign.go
@@ -1,8 +1,14 @@
 package support
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/md5"
+	"crypto/rand"
+	"encoding/base64"
 	"encoding/hex"
+	"fmt"
+	"io"
 )
 
 type Sign struct {
@@ -24,4 +30,57 @@ func (s *Sign) GetMd5() string {
 	cipherStr := h.Sum(nil)
 
 	return hex.EncodeToString(cipherStr)
+}
+
+// encrypt string to base64 crypto using AES
+func (s *Sign) AesEncrypt() string {
+
+	key := []byte(s.Key)
+	plaintext := []byte(s.Src)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	// convert to base64
+	return base64.URLEncoding.EncodeToString(ciphertext)
+}
+
+// decrypt from base64 to decrypted string
+func (s *Sign) AesDecrypt() string {
+
+	key := []byte(s.Key)
+	ciphertext, _ := base64.URLEncoding.DecodeString(s.Src)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	if len(ciphertext) < aes.BlockSize {
+		panic("ciphertext too short")
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	// XORKeyStream can work in-place if the two arguments are the same.
+	stream.XORKeyStream(ciphertext, ciphertext)
+
+	return fmt.Sprintf("%s", ciphertext)
 }
--- app/tmp/main.go
+++ app/tmp/main.go
@@ -2,16 +2,16 @@
 package main
 
 import (
-	"flag"
-	"reflect"
-	"github.com/revel/revel"
 	_ "blog/app"
 	controllers "blog/app/controllers"
 	tests "blog/tests"
+	"flag"
 	controllers1 "github.com/revel/modules/static/app/controllers"
 	_ "github.com/revel/modules/testrunner/app"
 	controllers0 "github.com/revel/modules/testrunner/app/controllers"
+	"github.com/revel/revel"
 	"github.com/revel/revel/testing"
+	"reflect"
 )
 
 var (
@@ -28,89 +28,76 @@ func main() {
 	flag.Parse()
 	revel.Init(*runMode, *importPath, *srcPath)
 	revel.INFO.Println("Running revel server")
-	
+
 	revel.RegisterController((*controllers.App)(nil),
 		[]*revel.MethodType{
 			&revel.MethodType{
 				Name: "Index",
-				Args: []*revel.MethodArg{ 
-				},
-				RenderArgNames: map[int][]string{ 
-					10: []string{ 
-					},
+				Args: []*revel.MethodArg{},
+				RenderArgNames: map[int][]string{
+					10: []string{},
 				},
 			},
-			
 		})
-	
+
 	revel.RegisterController((*controllers0.TestRunner)(nil),
 		[]*revel.MethodType{
 			&revel.MethodType{
 				Name: "Index",
-				Args: []*revel.MethodArg{ 
-				},
-				RenderArgNames: map[int][]string{ 
-					72: []string{ 
+				Args: []*revel.MethodArg{},
+				RenderArgNames: map[int][]string{
+					72: []string{
 						"testSuites",
 					},
 				},
 			},
 			&revel.MethodType{
 				Name: "Suite",
-				Args: []*revel.MethodArg{ 
-					&revel.MethodArg{Name: "suite", Type: reflect.TypeOf((*string)(nil)) },
+				Args: []*revel.MethodArg{
+					&revel.MethodArg{Name: "suite", Type: reflect.TypeOf((*string)(nil))},
 				},
-				RenderArgNames: map[int][]string{ 
-				},
+				RenderArgNames: map[int][]string{},
 			},
 			&revel.MethodType{
 				Name: "Run",
-				Args: []*revel.MethodArg{ 
-					&revel.MethodArg{Name: "suite", Type: reflect.TypeOf((*string)(nil)) },
-					&revel.MethodArg{Name: "test", Type: reflect.TypeOf((*string)(nil)) },
+				Args: []*revel.MethodArg{
+					&revel.MethodArg{Name: "suite", Type: reflect.TypeOf((*string)(nil))},
+					&revel.MethodArg{Name: "test", Type: reflect.TypeOf((*string)(nil))},
 				},
-				RenderArgNames: map[int][]string{ 
-					125: []string{ 
-					},
+				RenderArgNames: map[int][]string{
+					125: []string{},
 				},
 			},
 			&revel.MethodType{
-				Name: "List",
-				Args: []*revel.MethodArg{ 
-				},
-				RenderArgNames: map[int][]string{ 
-				},
+				Name:           "List",
+				Args:           []*revel.MethodArg{},
+				RenderArgNames: map[int][]string{},
 			},
-			
 		})
-	
+
 	revel.RegisterController((*controllers1.Static)(nil),
 		[]*revel.MethodType{
 			&revel.MethodType{
 				Name: "Serve",
-				Args: []*revel.MethodArg{ 
-					&revel.MethodArg{Name: "prefix", Type: reflect.TypeOf((*string)(nil)) },
-					&revel.MethodArg{Name: "filepath", Type: reflect.TypeOf((*string)(nil)) },
+				Args: []*revel.MethodArg{
+					&revel.MethodArg{Name: "prefix", Type: reflect.TypeOf((*string)(nil))},
+					&revel.MethodArg{Name: "filepath", Type: reflect.TypeOf((*string)(nil))},
 				},
-				RenderArgNames: map[int][]string{ 
-				},
+				RenderArgNames: map[int][]string{},
 			},
 			&revel.MethodType{
 				Name: "ServeModule",
-				Args: []*revel.MethodArg{ 
-					&revel.MethodArg{Name: "moduleName", Type: reflect.TypeOf((*string)(nil)) },
-					&revel.MethodArg{Name: "prefix", Type: reflect.TypeOf((*string)(nil)) },
-					&revel.MethodArg{Name: "filepath", Type: reflect.TypeOf((*string)(nil)) },
+				Args: []*revel.MethodArg{
+					&revel.MethodArg{Name: "moduleName", Type: reflect.TypeOf((*string)(nil))},
+					&revel.MethodArg{Name: "prefix", Type: reflect.TypeOf((*string)(nil))},
+					&revel.MethodArg{Name: "filepath", Type: reflect.TypeOf((*string)(nil))},
 				},
-				RenderArgNames: map[int][]string{ 
-				},
+				RenderArgNames: map[int][]string{},
 			},
-			
 		})
-	
-	revel.DefaultValidationKeys = map[string]map[int]string{ 
-	}
-	testing.TestSuites = []interface{}{ 
+
+	revel.DefaultValidationKeys = map[string]map[int]string{}
+	testing.TestSuites = []interface{}{
 		(*tests.AppTest)(nil),
 	}
 

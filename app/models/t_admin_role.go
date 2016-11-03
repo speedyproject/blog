e9bc8ba3ddf9331dc6bb8b05f50aae0022cf036e
Nico W. J <gksqlsdy@gmail.com>
2016-11-04 05:21:17 +0800

    Add admin role

--- app/models/t_admin_role.go
+++ app/models/t_admin_role.go
@@ -1,8 +1,27 @@
 package models
 
+import "blog/app/support"
+import "github.com/revel/revel"
+
 //AdminRole model
 type AdminRole struct {
 	Id       int    `xorm:"not null pk autoincr INT(11)"`
 	RoleDes  string `xorm:"VARCHAR(20)"`
 	RoleType int    `xorm:"INT(11)"`
+}
+
+//FindByType -> find role data for type
+func (a *AdminRole) FindByType(id int) (*AdminRole, string) {
+
+	ar := new(AdminRole)
+
+	_, err := support.Xorm.Where("role_type = ?", id).Get(&ar)
+
+	if err != nil {
+		return ar, err.Error()
+	}
+
+	revel.INFO.Println(ar)
+
+	return ar, ""
 }

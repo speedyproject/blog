93aa6ed176304a752603c1435cd01371e0c54cb4
Nico W. J <gksqlsdy@gmail.com>
2016-11-11 20:35:44 +0800

    main page add site info

--- app/controllers/main.go
+++ app/controllers/main.go
@@ -1,14 +1,31 @@
 package controllers
 
 import "github.com/revel/revel"
+import "blog/app/models"
 
-//Main Controller.
+//Main controller.
 type Main struct {
 	*revel.Controller
+}
+
+//SiteInfo model
+type SiteInfo struct {
+	Title     string
+	SubTitle  string
+	Copyright string
 }
 
 //Main page.
 func (m *Main) Main() revel.Result {
 
-	return m.Render()
+	set := new(models.Setting)
+	set.Key = "site-title"
+	title, _ := set.Get()
+	set.Key = "site-subtitle"
+	subtitle, _ := set.Get()
+	set.Key = "site-foot"
+	copyr, _ := set.Get()
+
+	info := &SiteInfo{Title: title, SubTitle: subtitle, Copyright: copyr}
+	return m.Render(info)
 }

66dfca7a14d53180ff33ad0674583d06ffd861ae
Laily <I@laily.net>
2016-11-19 19:08:57 +0800

    add full-text search

--- app/controllers/search.go
+++ app/controllers/search.go
@@ -1,6 +1,9 @@
 package controllers
 
 import "github.com/revel/revel"
+import "blog/app/service"
+import "blog/app/models"
+import "fmt"
 
 type Search struct {
 	*revel.Controller
@@ -8,5 +11,16 @@ type Search struct {
 
 func (s *Search) Index(q string) revel.Result {
 	s.RenderArgs["keywords"] = q
+	ids := service.FullTextSearch(q)
+	blogs := make([]*models.Blogger, 0)
+	for _, v := range ids {
+		blogModel := &models.Blogger{Id: v}
+		b, err := blogModel.FindById()
+		if err == nil {
+			blogs = append(blogs, b)
+		}
+	}
+	fmt.Println("blogs are: ", blogs)
+	s.RenderArgs["blogs"] = blogs
 	return s.RenderTemplate("Search/Index.html")
 }

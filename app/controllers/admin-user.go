2178e37668c8027073c25374f642a32b34b80b66
Laily <I@laily.net>
2016-12-08 21:22:01 +0800

    add comment

--- app/controllers/admin-user.go
+++ app/controllers/admin-user.go
@@ -2,19 +2,20 @@ package controllers
 
 import (
 	"blog/app/models"
-	"strings"
-
 	"strconv"
+	"strings"
 
 	"github.com/revel/revel"
 )
 
 // User for User Controller
+// 用户管理控制器
 type User struct {
 	Admin
 }
 
 // Main to list all users
+// 用户管理首页，列出所有用户
 func (user *User) Main() revel.Result {
 	admin := models.Admin{}
 	users, err := admin.List()
@@ -27,6 +28,7 @@ func (user *User) Main() revel.Result {
 }
 
 // Edit User
+// 编辑用户
 func (user *User) Edit(id int64) revel.Result {
 	u := &models.Admin{}
 	u, err := u.GetUserByID(id)
@@ -37,6 +39,8 @@ func (user *User) Edit(id int64) revel.Result {
 	return user.RenderTemplate("Admin/User/Edit.html")
 }
 
+// EditHandler to update user info
+// 处理编辑用户的方法
 func (user *User) EditHandler(username, nickname, password, email string, group int, id int64) revel.Result {
 	u := &models.Admin{Name: username, Nickname: nickname, Email: email, Passwd: password, RoleId: group}
 	_, err := u.UpdateAdmin(id, u)
@@ -47,11 +51,13 @@ func (user *User) EditHandler(username, nickname, password, email string, group 
 }
 
 // Create user
+// 创建用户页面
 func (user *User) Create() revel.Result {
 	return user.RenderTemplate("Admin/User/Create.html")
 }
 
 // CreateHandler for do create user
+// 处理创建用户的请求
 func (user *User) CreateHandler(username, nickname, password, email string, group int) revel.Result {
 	revel.INFO.Printf("%s,%s,%s,%s,%d", username, nickname, password, email, group)
 	u := &models.Admin{Name: username, Nickname: nickname, Email: email, Passwd: password, RoleId: group}
@@ -63,6 +69,7 @@ func (user *User) CreateHandler(username, nickname, password, email string, grou
 }
 
 // Delete a user
+// 删除用户
 func (user *User) Delete(ids string) revel.Result {
 	arr := strings.Split(ids, ",")
 	u := new(models.Admin)

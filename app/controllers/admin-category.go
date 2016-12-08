d0923ab74609d8c68ea4e057e8c654957e42bd89
Laily <I@laily.net>
2016-12-08 21:18:56 +0800

    add comment

--- app/controllers/admin-category.go
+++ app/controllers/admin-category.go
@@ -3,10 +3,14 @@ package controllers
 import "github.com/revel/revel"
 import "blog/app/models"
 
+// Category for blog, admin user can access
+// 博客分类，后台用户操作
 type Category struct {
 	Admin
 }
 
+// Index page for manage category
+// 后台管理博客分类的首页
 func (c *Category) Index() revel.Result {
 	category := new(models.Category)
 	c.RenderArgs["categorys"] = category.FindAll()

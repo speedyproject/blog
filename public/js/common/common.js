a981d767d0a81d69a66f665ddfbd753f0d5f9ae6
Laily <I@laily.net>
2017-02-25 16:53:49 +0800

    添加搜索快捷键

--- public/js/common/common.js
+++ public/js/common/common.js
@@ -1,5 +1,20 @@
 var Common ={
+    init: function(){
+        Common.initSearchKey();
+    },
+    initSearchKey: function(){
+        $("#search-key").keypress(function(e){
+            var k = e.which;
+            if(k == 13){
+                Common.toSearch();
+            }
+        })
+    },
     toSearch: function(){
         location.href="/search?q="+$("#search-key").val();
     }
 }
+
+$(function(){
+    Common.init();
+})
